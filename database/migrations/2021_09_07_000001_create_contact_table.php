2f0fb14bb1151b7d24596b9905d9b2fee1244f3b
tiago-gomes <email@example.com>
2021-09-07 19:07:37 +0100

    Add migration file

---
+++ database/migrations/2021_09_07_000001_create_contact_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('firstName', 75);
+            $table->string('email', 125)->unique();
+            $table->string('phone', 12);
+            $table->string('leadSource', 254);
+            $table->string('salesforce_external_id', 254)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact');
+    }
+}
