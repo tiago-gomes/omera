efcfec95ccb69b43d49b8fdc07edbdb8f3f3056b
tiago-gomes <email@example.com>
2021-09-07 19:10:54 +0100

    Add a bind to the Application Container for the ContactRepository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Domain\Repository\ContactRepository;
+use App\Domain\Repository\Contract\ContactRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+       $this->app->bind(
+           ContactRepositoryInterface::class, ContactRepository::class
+       );
     }
 }
