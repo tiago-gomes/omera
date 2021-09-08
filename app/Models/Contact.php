dea71da83790306d7c136c81a8bb334fa9394367
tiago-gomes <email@example.com>
2021-09-08 09:31:28 +0100

    update contact model with ide-helper

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -12,6 +12,24 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|Contact newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Contact query()
  * @mixin \Eloquent
+ * @property int $id
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $email
+ * @property string $phone
+ * @property string $lead_source
+ * @property string|null $salesforce_external_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Contact whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Contact whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Contact whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Contact whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Contact whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Contact whereLeadSource($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Contact wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Contact whereSalesforceExternalId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Contact whereUpdatedAt($value)
  */
 class Contact extends Model
 {
@@ -23,10 +41,11 @@ class Contact extends Model
      * @var string[]
      */
     protected $fillable = [
-        'firstName',
+        'first_name',
+        'last_name',
         'email',
         'phone',
-        'leadSource',
+        'lead_source',
         'salesforce_external_id',
     ];
 }
