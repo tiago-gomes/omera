9133dcd0a2f8971d1d28a44ddb4ae6725a024201
tiago-gomes <email@example.com>
2021-09-08 09:32:06 +0100

    Updated EventServiceProvider.php

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -1,25 +1,37 @@
 <?php
-
-namespace App\Providers;
-
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
-use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
-
-class EventServiceProvider extends ServiceProvider
-{
+  
+  namespace App\Providers;
+  
+  use App\Events\SalesForceCreateEvent;
+  use App\Events\SalesForceDeleteEvent;
+  use App\Events\SalesForceUpdateEvent;
+  use App\Listeners\SalesForceCreateListener;
+  use App\Listeners\SalesForceDeleteListener;
+  use App\Listeners\SalesForceUpdateListener;
+  use Illuminate\Auth\Events\Registered;
+  use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+  use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+  use Illuminate\Support\Facades\Event;
+  
+  class EventServiceProvider extends ServiceProvider
+  {
     /**
      * The event listener mappings for the application.
      *
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
+      SalesForceCreateEvent::class => [
+        SalesForceCreateListener::class,
+      ],
+      SalesForceUpdateEvent::class => [
+        SalesForceUpdateListener::class
+      ],
+      SalesForceDeleteEvent::class => [
+        SalesForceDeleteListener::class
+      ],
     ];
-
+    
     /**
      * Register any events for your application.
      *
@@ -27,6 +39,6 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+      //
     }
-}
+  }
