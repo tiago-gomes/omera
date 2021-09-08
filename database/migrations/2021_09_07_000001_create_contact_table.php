457337d074c66066864a12cb3a38d75dbcad73dd
tiago-gomes <email@example.com>
2021-09-08 10:25:46 +0100

    updated migration

--- database/migrations/2021_09_07_000001_create_contact_table.php
+++ database/migrations/2021_09_07_000001_create_contact_table.php
@@ -15,11 +15,11 @@ class CreateContactTable extends Migration
     {
         Schema::create('contact', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('first_name', 75);
-            $table->string('last_name', 75);
-            $table->string('email', 125)->unique();
-            $table->string('phone', 12);
-            $table->string('lead_source', 254);
+            $table->string('first_name', 75)->nullable();
+            $table->string('last_name', 75)->nullable();
+            $table->string('email', 125)->unique()->nullable();
+            $table->string('phone', 12)->nullable();
+            $table->string('lead_source', 254)->nullable();
             $table->string('salesforce_external_id', 254)->unique()->nullable();
             $table->timestamps();
             
